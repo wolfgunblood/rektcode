bad9ee869e0d6737256837fe4cf315e9a0d3b409
wolfgunblood <jalajdorai100@gmail.com>
2024-03-16 15:47:03 +0530

    Initialize App with next js and shadcn

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="text-red-500">hello world</h1>
+    <Button>Click Me!!!</Button>
+    </>
+  );
+}
